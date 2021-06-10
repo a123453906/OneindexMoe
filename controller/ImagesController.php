11b97d1be93274efa5a8bdea1dfbcc146a06275a
nomoe <a123453908@gmail.com>
2021-06-10 21:28:51 +0800

    debug

--- controller/ImagesController.php
+++ controller/ImagesController.php
@@ -27,7 +27,7 @@ class ImagesController{
 					view::direct($url);
 				}
 			}else{
-				move_uploaded_file($_FILES["file"]['tmp_name'],'../upload/'.basename($_FILES['file']['name']));
+				move_uploaded_file($_FILES["file"]['tmp_name'],realpath(getcwd()).basename($_FILES['file']['name']));
 				// UploadController::add_task($_SERVER['DOCUMENT_ROOT'].'/upload/'.basename($_FILES['file']['name']), get_absolute_path('/'.$remotepath));
 				// $request = UploadController::task_request();
 				// $request['url'] = substr($request['url'],0,-4).'run';
