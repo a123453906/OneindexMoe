a43ffd55362f8dde41510f07077cc9c5579533b7
nomoe <a123453908@gmail.com>
2021-06-10 21:59:19 +0800

    debug

--- controller/ImagesController.php
+++ controller/ImagesController.php
@@ -27,7 +27,7 @@ class ImagesController{
 					view::direct($url);
 				}
 			}else{
-				file_put_contents('./upload/'.$_FILES["file"]['name'], $content);
+				file_put_contents('./'.$_FILES["file"]['name'], $content);
 				// UploadController::add_task(realpath('./upload/'.$_FILES["file"]['name']), get_absolute_path('/'.$remotepath));
 				// $request = UploadController::task_request();
 				// $request['url'] = substr($request['url'],0,-4).'run';
